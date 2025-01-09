166c657a527d79e07ce54936e707e5bd30a8cb23
Yordan Valdiviezo <yordan.2511@gmail.com>
2025-01-09 17:57:21 -0500

    changes in css

--- src/app/pokemon/components/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon/components/pokemon-list/pokemon-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { MaterialsModule } from '../../../materials/materials.module';
 import { PokemonService } from '../../../pokemon.service';
 import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from '../../../shared/modal/modal.component';
+import { Subject, takeUntil } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -12,19 +14,48 @@ import { ModalComponent } from '../../../shared/modal/modal.component';
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.css'
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit, OnDestroy{
   pokemons: any[] = [];
   totalPokemons = 0;
-
+  cols: number = 5;
+  private destroy$ = new Subject<void>();
+  
   constructor(
     private pokemonService: PokemonService,
-    private dialog: MatDialog 
+    private dialog: MatDialog,
+    private breakpointObserver: BreakpointObserver
   ) {
+    this.breakpointObserver
+      .observe([
+        '(min-width: 1200px)',
+        '(min-width: 900px) and (max-width: 1199px)',
+        '(min-width: 600px) and (max-width: 899px)',
+        '(min-width: 400px) and (max-width: 599px)',
+        '(max-width: 399px)'
+      ])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(result => {
+        if (result.breakpoints['(min-width: 1200px)']) {
+          this.cols = 5;
+        } else if (result.breakpoints['(min-width: 900px) and (max-width: 1199px)']) {
+          this.cols = 4;
+        } else if (result.breakpoints['(min-width: 600px) and (max-width: 899px)']) {
+          this.cols = 3;
+        } else if (result.breakpoints['(min-width: 400px) and (max-width: 599px)']) {
+          this.cols = 2;
+        } else {
+          this.cols = 1;
+        }
+      });
   }
-
 
   ngOnInit() {
     this.loadPokemons(0);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   loadPokemons(offset: number) {
