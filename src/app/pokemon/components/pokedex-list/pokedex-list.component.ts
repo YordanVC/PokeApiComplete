477ed25ea63528aeed16470394a9ccfd795983be
Yordan <yordan.2511@gmail.com>
2025-01-16 16:27:43 -0500

    imporve css button

---
+++ src/app/pokemon/components/pokedex-list/pokedex-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedex-list',
+  imports: [],
+  templateUrl: './pokedex-list.component.html',
+  styleUrl: './pokedex-list.component.css'
+})
+export class PokedexListComponent {
+
+}
